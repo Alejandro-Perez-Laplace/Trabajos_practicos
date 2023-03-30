e3fd4889270e9dfe921ae30e13f059693160fdf9
Alejandro-Perez-Laplace <127541475+Alejandro-Perez-Laplace@users.noreply.github.com>
2023-03-30 14:38:01 -0300

    Update Ejercicio_6.sh

--- 04-señales/Ejercicio_6.sh
+++ 04-señales/Ejercicio_6.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 ls /home/alejandro/Desktop/TDIII/practica/04-señales/ # Voy a ver la lista de archivos en la ubicacion del archivo sig04.c
 cd /home/alejandro/Desktop/TDIII/practica/04-señales/ # Se posiciona en la carpeta del archivo donde esta sig04.c
-gcc -o sig04 sig04.c # Compila el archivo sig04.c
-./sig04
-
+gcc -o sig004 sig004.c # Compila el archivo sig04.c
+./sig004
