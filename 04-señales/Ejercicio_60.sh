f026c54d91a7a7fd6df7cca3fafd08b9c29cc8d4
Alejandro-Perez-Laplace <127541475+Alejandro-Perez-Laplace@users.noreply.github.com>
2023-03-30 14:38:37 -0300

    Delete Ejercicio_60.sh

--- 04-señales/Ejercicio_60.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ls /home/alejandro/Desktop/TDIII/practica/04-señales/ # Voy a ver la lista de archivos en la ubicacion del archivo sig004.c
-cd /home/alejandro/Desktop/TDIII/practica/04-señales/ # Se posiciona en la carpeta del archivo donde esta sig004.c
-gcc -o sig004 sig004.c # Compila el archivo sig04.c
-./sig004
-
