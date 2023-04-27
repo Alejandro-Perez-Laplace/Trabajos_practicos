7c9b921057286724c53ea5a7628c8a2d5759d584
Alejandro-Perez-Laplace <127541475+Alejandro-Perez-Laplace@users.noreply.github.com>
2023-04-27 16:29:54 -0300

    Update pipe08.py

--- 05-Tuberias/pipe08.py
+++ 05-Tuberias/pipe08.py
@@ -1,56 +1,69 @@
-from math import radians, sin, cos, sqrt, atan2
 import csv
-import os
-from multiprocessing import Process, Pipe
+import math
+import multiprocessing
+from multiprocessing import Pipe
 
 
-def haversine(lat1, lon1, lat2, lon2): 
-    R = 6371.0 # Radio de la Tierra en km
-    dLat = radians(lat2 - lat1)
-    dLon = radians(lon2 - lon1)
-    lat1 = radians(lat1)
-    lat2 = radians(lat2)
-    a = sin(dLat / 2)**2 + cos(lat1) * cos(lat2) * sin(dLon / 2)**2
-    c = 2 * atan2(sqrt(a), sqrt(1 - a))
-    return R * c
+def read_gps_data(filename):
+    with open(filename, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            latitude = float(row[0])
+            longitude = float(row[1])
+            yield latitude, longitude
 
- 
-def calcular_distancias(proceso_hijo_pipe): 
-   
-    lat1, lon1 = proceso_hijo_pipe.recv()
-    dist_total = 0
-    
-    while True:
-       
-        try:
-            lat2, lon2 = proceso_hijo_pipe.recv()
-        except EOFError:
-            
-            break
-        
-        dist_tramo = haversine(lat1, lon1, lat2, lon2)
-        
-        dist_total += dist_tramo
-       
-        print("Distancia del tramo:",dist_tramo," km")
-       
-        lat1, lon1 = lat2, lon2
-    
-    print("Distancia total recorrida:",dist_total," km")
+
+def haversine_distance(lat1, lon1, lat2, lon2):
+    R = 6371.0  # Radio de la Tierra en kilómetros
+
+    lat1_rad = math.radians(lat1)
+    lon1_rad = math.radians(lon1)
+    lat2_rad = math.radians(lat2)
+    lon2_rad = math.radians(lon2)
+
+    dlon = lon2_rad - lon1_rad
+    dlat = lat2_rad - lat1_rad
+
+    a = math.sin(dlat / 2)**2 + math.cos(lat1_rad) * math.cos(lat2_rad) * math.sin(dlon / 2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+    distance = R * c
+    return distance
+
+
+def calculate_distance(conn, gps_data):
+    total_distance = 0
+    previous_lat = None
+    previous_lon = None
+
+    for lat, lon in gps_data:
+        if previous_lat is not None and previous_lon is not None:
+            distance = haversine_distance(previous_lat, previous_lon, lat, lon)
+            total_distance += distance
+            conn.send(distance)
+
+        previous_lat = lat
+        previous_lon = lon
+
+    conn.send(total_distance)
+    conn.close()
+
 
 if __name__ == '__main__':
-    ruta_archivo = os.path.abspath("gps_data.csv")
-    padre_pipe, hijo_pipe = Pipe()
-    proceso_hijo = Process(target=calcular_distancias, args=(hijo_pipe,))
-    proceso_hijo.start()
-    with open(ruta_archivo, "r") as archivo_csv:
-        lector_csv = csv.reader(archivo_csv)
-        lat_idx = 0
-        lon_idx = 1
-        alt_idx = 2
-        for fila in lector_csv:
-            lat = float(fila[lat_idx])
-            lon = float(fila[lon_idx])
-            padre_pipe.send((lat, lon))
-    padre_pipe.close()  
-    proceso_hijo.join()
+    gps_data = list(read_gps_data('gps_data.csv'))
+    parent_conn, child_conn = Pipe()
+
+    p = multiprocessing.Process(target=calculate_distance, args=(child_conn, gps_data))
+    p.start()
+
+    while True:
+        try:
+            distance = parent_conn.recv()
+            print(f"Distancia: {distance:.2f} km")
+        except EOFError:
+            break
+
+    total_distance = parent_conn.recv()
+    print(f"Distancia total recorrida: {total_distance:.2f} km")
+
+    p.join()
