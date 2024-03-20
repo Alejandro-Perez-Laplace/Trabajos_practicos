b2049872e8b00cb20c2d47f054d6220352279e52
Alejandro-Perez-Laplace <127541475+Alejandro-Perez-Laplace@users.noreply.github.com>
2024-03-20 18:18:23 -0300

    Update Ejercicio_1.sh
    version 2024

--- 01-Linea-de-comando/Ejercicio_1.sh
+++ 01-Linea-de-comando/Ejercicio_1.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
-alejandro@laplace:~/Desktop$ mkdir ejercicio_bash/ #se crea la carpeta
-alejandro@laplace:~/Desktop$ ls #Comprobacion 
-ejercicio_bash
-alejandro@laplace:~/Desktop$ cd ejercicio_bash/ #se crea la carpeta ejercicio_bash
-alejandro@laplace:~/Desktop/ejercicio_bash$ nano ejercicio.txt #se crea el archivo ejercicio_.txt
-alejandro@laplace:~/Desktop/ejercicio_bash$ ls
-ejercicio.txt
-alejandro@laplace:~/Desktop/ejercicio_bash$ echo "Hola mundo " >  ejercicio.txt # se escribe en el archivo "Hola mundo"
-alejandro@laplace:~/Desktop/ejercicio_bash$ cat ejercicio.txt #Se verifica el paso anterior
-Hola mundo 
-alejandro@laplace:~/Desktop/ejercicio_bash$ mkdir subcarpeta/ #se crea "subcarpeta"
-alejandro@laplace:~/Desktop/ejercicio_bash$ ls # comprobacion
-ejercicio.txt  subcarpeta
-alejandro@laplace:~/Desktop/ejercicio_bash$ cp ejercicio.txt subcarpeta/	#se copia ejercicio.txt a subcarpeta
-alejandro@laplace:~/Desktop/ejercicio_bash$ ls subcarpeta/ #comprobacion
-ejercicio.txt
+mkdir /home/alejandro/Desktop/Ejercicio_bash/ #se crea la carpeta
+ls /home/alejandro/Desktop/ #Comprobacion 
+cd /home/alejandro/Desktop/Ejercicio_bash/ #se ubaca dentro de la carpeta
+ls
+echo "Hola mundo " >  ejercicio.txt # se crea y escribe en el archivo "Hola mundo"
+cat ejercicio.txt #Se verifica el paso anterior si hay un "Hola mundo"
+mkdir subcarpeta/ #se crea "subcarpeta"
+ls # comprobacion por consola
+cp ejercicio.txt subcarpeta/	#se copia ejercicio.txt a subcarpeta
+ls subcarpeta/ #comprobacion
