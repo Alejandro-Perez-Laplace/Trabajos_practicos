d73c5b8dc0ecc941373338798cb3e54058645eab
Alejandro-Perez-Laplace <127541475+Alejandro-Perez-Laplace@users.noreply.github.com>
2024-04-10 18:17:38 -0300

    Add files via upload

---
+++ 03-Procesos/Ejercicio_1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/alejandro/Desktop/2024/03-procesos/ #se ubica dentro de la carpeta
+gcc -o proc_01 proc_01.c
+./proc_01
---
+++ 03-Procesos/Ejercicio_5.sh
@@ -0,0 +1,3 @@
+cd /home/alejandro/Desktop/2024/03-procesos/ #se ubica dentro de la carpeta
+gcc -o proc_05 proc_05.c
+./proc_05
