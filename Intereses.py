c8f0b8bcdece11ffc0047b2d5c06b55f3e2a0949
Alejandro-Perez-Laplace <127541475+Alejandro-Perez-Laplace@users.noreply.github.com>
2024-04-23 20:22:28 -0300

    Update Intereses.py

--- Intereses.py
+++ Intereses.py
@@ -1,6 +1,6 @@
 def main():
     
-    principal = int(input("Ingrese el principal: ")) # Solicitud de datos al usuario
+    capital = int(input("Ingrese el capital inicial: ")) # Solicitud de datos al usuario
     tasa_interes_anual = float(input("Ingrese la tasa de interés anual (en %): "))
     veces_capitalizacion_anual = int(input("Ingrese el número de veces que se capitaliza el interés al año: "))
     años_inversion = int(input("Ingrese el número de años que permanecerá la inversión: "))
@@ -8,10 +8,10 @@ def main():
     # Convertir la tasa de interés anual de porcentaje a decimal
     tasa_interes_decimal = tasa_interes_anual / 100
 
-    # Calcular el monto de interés compuesto
-    monto_acumulado = principal * (1 + tasa_interes_decimal / veces_capitalizacion_anual) ** (veces_capitalizacion_anual * años_inversion)
+    # Interés compuesto
+    monto_acumulado = capital * (1 + tasa_interes_decimal / veces_capitalizacion_anual) ** (veces_capitalizacion_anual * años_inversion)
 
-    # Imprimir el monto acumulado después de t años con dos decimales
+    # Presentar el monto acumulado después de t años con dos decimales
     print("El monto acumulado después de {} años es: {:.2f}".format(años_inversion, monto_acumulado))
 
 
