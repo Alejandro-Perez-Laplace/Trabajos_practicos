96d42ef37f095a99fa03460d907a929afe4d4317
Alejandro-Perez-Laplace <127541475+Alejandro-Perez-Laplace@users.noreply.github.com>
2024-04-29 14:47:39 -0300

    Update Ejercicio_python_1_1.py

--- 05-Tuberias/2024/Ejercicio_python_1_1.py
+++ 05-Tuberias/2024/Ejercicio_python_1_1.py
@@ -1,25 +1,23 @@
 import os
 
-# Create a pipe
-read_pipe, write_pipe = os.pipe()
+read_pipe, write_pipe = os.pipe()# Creacion de pipe
 
-# Create a child process
-child_pid = os.fork()
+child_pid = os.fork()# Creacion del proceso hijo
 
 if child_pid == 0:
-    # This is the child process
-    os.close(write_pipe)
+    # Proceso hijo
+    os.close(write_pipe)#se cierra la tuberia de escritura desde el proceso hijo
     message = os.read(read_pipe, 1024)
     print(f'Received message in child process: {message.decode()}')
-    os.close(read_pipe)
+    os.close(read_pipe)#se cierra la tuberia de lectura desde el proceso hijo
     exit(0)
 else:
-    # This is the parent process
-    os.close(read_pipe)
-    #message = b'Hello from parent process!'
+    # Proceso padre
+    os.close(read_pipe)#se cierra la tuberia de lectura desde el proceso padre
+    #mensaje = b'Hello from parent process!'
     message = f'Hello from parent process {os.getpid()}!'
     #os.write(write_pipe, message)
     os.write(write_pipe, message.encode())
-    os.close(write_pipe)
+    os.close(write_pipe)#se cierra la tuberia de escritura desde el proceso padre
     pid, status = os.waitpid(child_pid, 0)
     print(f'Child process {child_pid} finished with status {status}')
