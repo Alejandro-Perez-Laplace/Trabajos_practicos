5fb3f4fc0435d9c7cfebca3426e8c9e081b564ac
Alejandro-Perez-Laplace <127541475+Alejandro-Perez-Laplace@users.noreply.github.com>
2024-06-23 20:48:13 -0300

    Update hilos01.c

--- 09-hilos/hilos01.c
+++ 09-hilos/hilos01.c
@@ -27,12 +27,26 @@ int main() {
     printf("Main creando el hilo nro %d\n", t);
         
     rc = pthread_create(&hilo[0], NULL, hola , (void *)(&t)  );
+//Algunos de los atributos que comparten los hilos son:
+//• ID del proceso. 
+//• ID del proceso padre.
+//• Descriptores de archivos abiertos.
+//• Credenciales de proceso  (usuario y ID de grupo).
+//Algunos de los atributos que no comparten los hilos son:
+//• ID de hilos (TID).
+//• Datos específicos del hilo.
+//• Pila del hilo.
      
     if (rc != 0){
          printf("ERROR; pthread_create() = %d\n", rc);
          exit(-1);        
          };
+//La creación del hilo es más rápida que la creación de un proceso (típicamente entre x10 y x100 más rápido). 
   
+//Generalmente es necesario implementar mecanismos de 
+//sincronización (mutex, semáforos) para que los hilos trabajen 
+//correctamente en forma colaborativa.
+
    printf("Espera a que termine hilo\n");
 
    pthread_join(hilo[0],NULL);
